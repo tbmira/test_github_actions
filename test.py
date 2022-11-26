577ff05eaaaa74683a3e4064d24a95781581d3f0
Thiago Mira <tbmira_prog@outlook.com>
2022-11-26 20:50:33 -0300

    foda-seeeeeeee

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ full_path = os.path.join(absolute_path, relative_path)
 
 file = open(full_path, 'w')# ...
 
-file.write("Olha eu aquiiii!")
+file.write("Olha eu aquiiii!") # ...
 print("Done ;)")
 
 f = open(full_path, 'r')
 print((f.readline()))
 
 file_2 = open(full_path.replace(".txt", "_2.txt", 'w'))
+a = [1,
+     2]
