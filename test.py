ca9b7513a75649f642b6f72497894bd43a814d4f
Thiago Mira <tbmira_prog@outlook.com>
2022-11-26 20:42:26 -0300

    foda-se

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import os
-
-
 absolute_path = os.path.dirname(__file__)
 relative_path = "outFile.txt"
 full_path = os.path.join(absolute_path, relative_path)
 
-file = open(full_path, 'w')
+file = open(full_path, 'w')# ...
 
 file.write("Olha eu aquiiii!")
 print("Done ;)")
