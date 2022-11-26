b5464516f5bad08cb6a2b3e504be65d731ebcad8
Thiago Mira <118115911+tbmira-prog@users.noreply.github.com>
2022-11-26 20:00:47 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ import os
 
 
 absolute_path = os.path.dirname(__file__)
-relative_path = 'outFile.txt'
+relative_path = "outFile.txt"
 full_path = os.path.join(absolute_path, relative_path)
 
 file = open(full_path, 'w')
 
-file.write('Olha eu aquiiii!')
-print('Done ;)')
+file.write("Olha eu aquiiii!")
+print("Done ;)")
 
 f = open(full_path, 'r')
 print((f.readline()))
 
-file_2 = open(full_path.replace('.txt', '_2.txt', 'w'))
+file_2 = open(full_path.replace(".txt", "_2.txt", 'w'))
